9172b8d2ddb70a66152f3d2b5df5e8882c398fff
Tăng Nguuuuuuu <59719049+omqn9696@users.noreply.github.com>
2025-10-01 09:24:53 +0700

    Update mail-coin.meta.js

--- mail-coin.meta.js
+++ mail-coin.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         mail coin
 // @namespace    https://github.com/Drayke/pixels-scripts
-// @version      1.3
+// @version      1.4
 // @description  Theo dõi mail coin, gửi về webhook khi collect mail trên Pixels.xyz
 // @author       Drayke
 // @match        *://play.pixels.xyz/*
