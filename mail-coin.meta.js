00a69ed7e74340fba9a4491e09a85a7782d02c0e
Tăng Nguuuuuuu <59719049+omqn9696@users.noreply.github.com>
2025-10-01 09:23:57 +0700

    Create mail-coin.meta.js

---
+++ mail-coin.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         mail coin
+// @namespace    https://github.com/Drayke/pixels-scripts
+// @version      1.3
+// @description  Theo dõi mail coin, gửi về webhook khi collect mail trên Pixels.xyz
+// @author       Drayke
+// @match        *://play.pixels.xyz/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/omqn9696/extendsionpga/refs/heads/main/mail-coin.meta.js
+// @downloadURL  https://raw.githubusercontent.com/omqn9696/extendsionpga/refs/heads/main/mail-coin.user.js
+// ==/UserScript==
