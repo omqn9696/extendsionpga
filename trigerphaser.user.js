63c0e75c4d61fa9bc4f538c8f4b559fb59147877
Tăng Nguuuuuuu <59719049+omqn9696@users.noreply.github.com>
2025-10-03 14:37:45 +0700

    Update trigerphaser.user.js

--- trigerphaser.user.js
+++ trigerphaser.user.js
@@ -5,6 +5,8 @@
 // @icon         https://play.pixels.xyz/favicon/favicon.ico
 // @author       Drayke
 // @match        *://play.pixels.xyz/*
+// @updateURL    https://raw.githubusercontent.com/omqn9696/extendsionpga/refs/heads/main/trigerphaser.user.js
+// @downloadURL  https://raw.githubusercontent.com/omqn9696/extendsionpga/refs/heads/main/trigerphaser.meta.js
 // @grant        none
 // ==/UserScript==
 
