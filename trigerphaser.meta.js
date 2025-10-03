dfb64874d8c5fe66544287863cd53f898f0e2577
Tăng Nguuuuuuu <59719049+omqn9696@users.noreply.github.com>
2025-10-03 11:06:50 +0700

    Create trigerphaser.meta.js

---
+++ trigerphaser.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Triger game event 
+// @version      1.3
+// @description  xàm
+// @icon         https://play.pixels.xyz/favicon/favicon.ico
+// @author       Drayke
+// @match        *://play.pixels.xyz/*
+// @grant        none
+// ==/UserScript==
